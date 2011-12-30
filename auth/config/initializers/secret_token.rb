084455f874c4a5345f67a4875a19e4e969bcf3ef
kuba <slunzok@gmail.com>
2011-12-30 15:46:28 +0100

    sample auth in rails 3.1.3

---
+++ auth/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '691de7b04451c80ac9d91fddf5bbf3c873fa872182f7ccf2a942b237831bf9af7359c0d5ea508c30be082eef8fc8064cd79c5caa8b1c76f9ff9c9dfd1f9bfdd7'
